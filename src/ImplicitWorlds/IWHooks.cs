409df254701cec28f6286ffc28612ffb416a99a4
TabbyFlux <itstabbyuwu@gmail.com>
2025-10-13 23:41:58 +0800

    First rooms of Narrow Horizon, bkg image created for IWNH

--- src/ImplicitWorlds/IWHooks.cs
+++ src/ImplicitWorlds/IWHooks.cs
@@ -6,11 +6,20 @@ namespace ImplicitWorlds
     {
         public static void Apply()
         {
-
+            On.RainWorldGame.Update += TestHook;
         }
         public static void Undo()
         {
-
+            On.RainWorldGame.Update -= TestHook;
+        }
+        private static void TestHook(On.RainWorldGame.orig_Update orig, RainWorldGame self)
+        {
+            orig(self);
+            if (Input.GetKey(KeyCode.J))
+            {
+                self.RealizedPlayerFollowedByCamera.room.PlaySound(SoundID.SS_AI_Give_The_Mark_Boom, 0f, 1f, 1f);
+                UnityEngine.Debug.Log("[ImplicitWorlds]: sound has been played!");
+            }
         }
     }
 }
